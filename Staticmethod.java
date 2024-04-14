d23acb0439c6c52e80ad2abbacbee42762b62b4b
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-15 00:56:14 +0530

    static method

---
+++ Staticmethod.java
@@ -0,0 +1,11 @@
+public class Staticmethod {
+
+    public static void printRecord()
+    {
+        System.out.println("Hello world");
+    }
+  public static void main(String [] x)
+  {
+      Staticmethod.printRecord();  //class level method.
+  }  
+}
