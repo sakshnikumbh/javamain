35e9fe279f6bf55fa31a87b0e71e06a14f6b7419
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 12:39:58 +0530

    float field

---
+++ Floatfeild.java
@@ -0,0 +1,11 @@
+public class Floatfeild {
+    public static void main(String [] x)
+  {
+       System.out.println(Float.BYTES);
+        System.out.println(Float.SIZE);
+        System.out.println(Float.MIN_VALUE);  
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.TYPE);
+
+  }
+}
