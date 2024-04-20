9fa25347190d97dbd16c27944706c41ab7c3a74a
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 10:39:25 +0530

    unboxing

---
+++ Unboxing.java
@@ -0,0 +1,10 @@
+public class Unboxing {
+   
+        public static void main(String... x)
+        {
+          String str="123";                // Wrapper class method
+           int data=Integer.parseInt(str);
+              System.out.println(data);
+        }
+    }
+
