e576fc09f44655c6549dbb5467763dd7d7e7267f
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 12:07:16 +0530

    bytefield

---
+++ Bytefield.java
@@ -0,0 +1,15 @@
+public class Bytefield {
+    
+
+    public static void main(String [] x)
+    {
+        System.out.println(Byte.BYTES);
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);  
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.TYPE);
+
+
+    }
+
+}
