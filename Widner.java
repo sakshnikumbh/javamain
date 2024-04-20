3db8b5097675cc645c44bf17fb87fc1fc50905e3
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 10:18:18 +0530

    widner

---
+++ Widner.java
@@ -0,0 +1,9 @@
+class Widner
+{
+    public static void main(String... x)
+    {
+        int data=123;
+        double data2=(double) data;   //widening -narrow type is converted into widner type.
+        System.out.println(data2);
+    }
+}
