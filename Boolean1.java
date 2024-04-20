87200e5e9706fba1d36e9ad1ddda46605b6d7e30
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 12:33:48 +0530

    boolean type field

---
+++ Boolean1.java
@@ -0,0 +1,9 @@
+public class Boolean1 {
+    public static void main(String [] x)
+    {
+        System.out.println(Boolean.TRUE);
+        System.out.println(Boolean.FALSE);
+        System.out.println(Boolean.TYPE);
+
+    }
+}
