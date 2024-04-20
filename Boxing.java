2c96cd1d1321ecd1a47ca8fd8a2a154aee2fbc62
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 10:35:44 +0530

    boxing

---
+++ Boxing.java
@@ -0,0 +1,8 @@
+public class Boxing {
+    public static void main(String... x)
+    {
+        int data=123;
+          String str=Integer.toString(data); // Wrapper class method 
+          System.out.println(str);
+    }
+}
