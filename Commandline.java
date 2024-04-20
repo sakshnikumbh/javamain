cede631ad3da6d1be372139753884cce6038feb3
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-20 10:57:13 +0530

    Commandline

---
+++ Commandline.java
@@ -0,0 +1,23 @@
+public class Commandline {
+        public static void main(String [] x)
+        {
+          System.out.println(x[0]);  //sakshi
+          System.out.println(x[1]);  //kamlesh
+          System.out.println(x[2]);  //nikumbh
+
+         
+
+          int value1=Integer.parseInt(x[3]);
+          double value2=Double.parseDouble(x[4]);
+          System.out.println(x[3]);
+          System.out.println(x[4]);
+
+        }
+
+
+
+    }
+
+
+   // Commandline argument is the information that is passed at runtime time from console as a argument .
+   // so this info is pass as argument and taken as input by the java program.
