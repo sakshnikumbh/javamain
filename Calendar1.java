1c5f67f399c8e4415d67e0eddb8aac090481e688
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-21 12:51:45 +0530

    final cal

--- Calendar1.java
+++ Calendar1.java
@@ -3,9 +3,20 @@ public class Calendar1 {
     public static void main(String [] x)
     {
         Calendar calendar= Calendar.getInstance();
-        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        
+        int day=calendar.get(Calendar.DATE);
         int month=calendar.get(Calendar.MONTH)+1;
         int year=calendar.get(Calendar.YEAR);
         System.out.println(day+"  "+month+"  "+year);
+
+        int hour=calendar.get(Calendar.HOUR_OF_DAY);
+        int min=calendar.get(Calendar.MINUTE);
+        int second=calendar.get(Calendar.SECOND);
+        int milli=calendar.get(Calendar.MILLISECOND);
+        int a=calendar.get(Calendar.AM);
+
+
+        System.out.println(hour+"  "+min+"  "+second+" "+milli+"  "+a);
+        
     }  
 }
