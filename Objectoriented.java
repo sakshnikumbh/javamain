0a93812698899e9a0156015736e4f75702cdbb32
sakshnikumbh <nikumbhsakshi362@gmail.com>
2024-04-21 19:33:48 +0530

    objectoriented

---
+++ Objectoriented.java
@@ -0,0 +1,18 @@
+class Date1{
+     int day;
+     int month;   //field of class or 
+     int year;     //they are called as instance level member of class int year;
+}
+
+
+
+public class Objectoriented {
+    public static void main(String [] x)
+    {
+       Date1 date =new Date1();    //instance of class
+    }
+}
+
+
+// instance get memory on heap - only the field (instance member) get memory on heap.
+// reference get memory on java stack
